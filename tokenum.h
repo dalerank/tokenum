d047402e53c8f414dd814760bf000099e1f03c3c
dalerank <dalerankn8@gmail.com>
2022-11-03 23:45:33 +0100

    Update tokenum.h

--- tokenum.h
+++ tokenum.h
@@ -1,12 +1,3 @@
-// Online C++ compiler to run C++ program online
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <typeinfo>
 //#include <cxxabi.h>
